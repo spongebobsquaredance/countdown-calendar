de9d9840da7fc6f957dfae8571ae8f124b666c29
Carrie MacDonald <carrie8783@gmail.com>
2021-10-21 15:34:50 -0400

    Added 24 days-worth of sample data; moved sample data into a separate file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
 import React from 'react'
+import { calendarDays } from './sample-data'
 import DayList from './components/DayList'
-
-const calendarDays = [
-  {
-    "id": 0,
-    "day_index": 0,
-    "day_has_started": true,
-    "day_checked": false,
-    "image_file_path": "https://via.placeholder.com/150",
-    "image_alt_text": "Movie poster Edward Scissorhands",
-    "product_name": "Edward Scissorhands",
-    "product_url": "https://www.hulu.com/movie/edward-scissorhands-c71d11de-32df-4fb5-a0f1-06e1e0a0d5aa",
-  },
-  {
-    "id": 1,
-    "day_index": 1,
-    "day_has_started": false,
-    "day_checked": false,
-    "image_file_path": "https://via.placeholder.com/150",
-    "image_alt_text": "Movie poster Corpse Bride",
-    "product_name": "Corpse Bride",
-    "product_url": "https://www.amazon.com/Burtons-Corpse-Bride-Johnny-Depp/dp/B000GOWSF0",
-  },
-  {
-    "id": 2,
-    "day_index": 2,
-    "day_has_started": false,
-    "day_checked": false,
-    "image_file_path": "https://via.placeholder.com/150",
-    "image_alt_text": "Movie poster Casper",
-    "product_name": "Casper",
-    "product_url": "https://www.amazon.com/Casper-Christina-Ricci/dp/B000I9S5SC",
-  }
-]
 
 export default function App() {
   return (
